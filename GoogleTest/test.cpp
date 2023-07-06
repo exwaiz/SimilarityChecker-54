60d44c911c9dd40e86f9cbcaac64f9dbb295cd06
dongjune.youn <exwaiz@live.com>
2023-07-06 11:18:40 +0900

    [feature] length : add some initial files

--- GoogleTest/test.cpp
+++ GoogleTest/test.cpp
@@ -1,4 +1,8 @@
 #include "pch.h"
+class SentenceTestFixture : public testing::Test {
+public:
+private:
+};
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
---
+++ SimilarityChecker-54/sentence.cpp
@@ -0,0 +1,10 @@
+
+class Sentence
+{
+public:
+	int getLengthScore(int p1, int p2)
+	{
+		return 0;
+	}
+private:
+};
